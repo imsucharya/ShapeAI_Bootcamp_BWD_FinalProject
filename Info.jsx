1cae0cc1aa2c968c5ee34c86227cfc9b875033e2
Sucharya Suchita Suresh Deshmukh <83875053+Imsucharya@users.noreply.github.com>
2021-06-26 18:41:50 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>This is an amazing bootcamp taken up by Shaurya Sinha. We covered everything from scratch including JavaScript, React.js, HTML.</p>
+    </div>
+  );
+}
+
+export default Info;
